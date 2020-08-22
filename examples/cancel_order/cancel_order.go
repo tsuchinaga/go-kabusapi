9f7febee819cb936078b2395cdffefe32391047f
tsuchinaga <woocun@gmail.com>
2020-08-22 09:37:17 +0900

    注文取消のexampleを動くように約定しない注文をいれるようにした

--- examples/cancel_order/cancel_order.go
+++ examples/cancel_order/cancel_order.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"gitlab.com/tsuchinaga/go-kabusapi/kabus"
 )
@@ -23,12 +24,34 @@ func main() {
 		token = req.Token
 	}
 
-	// TODO 注文処理を入れる
 	orderID := ""
 	{
-
+		res, err := kabus.NewSendOrderRequester(token, isProd).Exec(kabus.SendOrderRequest{
+			Password:           password,
+			Symbol:             "1320",
+			Exchange:           kabus.ExchangeToushou,
+			SecurityType:       kabus.SecurityTypeKabu,
+			Side:               kabus.SideBuy,
+			CashMargin:         kabus.CashMarginCash,
+			MarginTradeType:    kabus.MarginTradeTypeUnspecified,
+			DelivType:          kabus.DelivTypeCash,
+			FundType:           kabus.FundTypeTransferMargin,
+			AccountType:        kabus.AccountTypeGeneral,
+			Qty:                1.0,
+			ClosePositionOrder: kabus.ClosePositionOrderUnspecified,
+			ClosePositions:     []kabus.ClosePosition{},
+			Price:              0,
+			ExpireDay:          kabus.YmdNUM{Time: time.Date(2020, 8, 24, 0, 0, 0, 0, time.Local)},
+			FrontOrderType:     kabus.FrontOrderTypeMarketCloseAfter, // 後場引成 = 約定しないように
+		})
+		if err != nil {
+			panic(err)
+		}
+		log.Println(res)
+		orderID = res.OrderID
 	}
 
+	time.Sleep(3 * time.Second) // kabuステーションで注文がどう動くかを確認するために待機時間を入れてます
 	{
 		res, err := kabus.NewCancelOrderRequester(token, isProd).Exec(kabus.CancelOrderRequest{
 			OrderID:  orderID,
