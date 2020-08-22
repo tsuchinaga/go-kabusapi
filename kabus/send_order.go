16ba27699abab462d5d846117af18a6b3318aa15
tsuchinaga <woocun@gmail.com>
2020-08-22 20:55:08 +0900

    信用返済時に決済順序か返済建玉指定のどちらかが出るよう修正

--- kabus/send_order.go
+++ kabus/send_order.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 )
 
-// SendOrderRequest - 注文発注のリクエストパラメータ TODO 返済時ClosePositionOrderとClosePositionsを同時に出してはいけないので対応する
+// SendOrderRequest - 注文発注のリクエストパラメータ
 type SendOrderRequest struct {
 	Password           string             `json:"Password"`           // 注文パスワード
 	Symbol             string             `json:"Symbol"`             // 銘柄コード
@@ -19,7 +19,91 @@ type SendOrderRequest struct {
 	AccountType        AccountType        `json:"AccountType"`        // 口座種別
 	Qty                int                `json:"Qty"`                // 注文数量
 	ClosePositionOrder ClosePositionOrder `json:"ClosePositionOrder"` // 決済順序 ※信用取引の場合必須
-	ClosePositions     []ClosePosition    `json:"ClosePositions"`     // 返済建玉指定
+	ClosePositions     []ClosePosition    `json:"ClosePositions"`     // 返済建玉指定 ※信用取引の場合必須
+	Price              int                `json:"Price"`              // 注文価格
+	ExpireDay          YmdNUM             `json:"ExpireDay"`          // 注文有効期限（年月日）
+	FrontOrderType     FrontOrderType     `json:"FrontOrderType"`     // 執行条件
+}
+
+func (r *SendOrderRequest) toJSON() ([]byte, error) {
+	if r.CashMargin == CashMarginMarginEntry || r.CashMargin == CashMarginMarginExit {
+		// 返済建玉指定に指定があれば決済順序なしのリクエストにする
+		if r.ClosePositions != nil && len(r.ClosePositions) > 0 {
+			return json.Marshal(sendOrderRequestWithoutClosePositionOrder{
+				Password:        r.Password,
+				Symbol:          r.Symbol,
+				Exchange:        r.Exchange,
+				SecurityType:    r.SecurityType,
+				Side:            r.Side,
+				CashMargin:      r.CashMargin,
+				MarginTradeType: r.MarginTradeType,
+				DelivType:       r.DelivType,
+				FundType:        r.FundType,
+				AccountType:     r.AccountType,
+				Qty:             r.Qty,
+				ClosePositions:  r.ClosePositions,
+				Price:           r.Price,
+				ExpireDay:       r.ExpireDay,
+				FrontOrderType:  r.FrontOrderType,
+			})
+		} else {
+			return json.Marshal(sendOrderRequestWithoutClosePositions{
+				Password:           r.Password,
+				Symbol:             r.Symbol,
+				Exchange:           r.Exchange,
+				SecurityType:       r.SecurityType,
+				Side:               r.Side,
+				CashMargin:         r.CashMargin,
+				MarginTradeType:    r.MarginTradeType,
+				DelivType:          r.DelivType,
+				FundType:           r.FundType,
+				AccountType:        r.AccountType,
+				Qty:                r.Qty,
+				ClosePositionOrder: r.ClosePositionOrder,
+				Price:              r.Price,
+				ExpireDay:          r.ExpireDay,
+				FrontOrderType:     r.FrontOrderType,
+			})
+		}
+	}
+
+	// 上記以外はそのまま出す
+	return json.Marshal(r)
+}
+
+// sendOrderRequestWithoutClosePositionOrder - 決済順序なしの注文発注のリクエストパラメータ
+type sendOrderRequestWithoutClosePositionOrder struct {
+	Password        string          `json:"Password"`        // 注文パスワード
+	Symbol          string          `json:"Symbol"`          // 銘柄コード
+	Exchange        Exchange        `json:"Exchange"`        // 市場コード
+	SecurityType    SecurityType    `json:"SecurityType"`    // 商品種別
+	Side            Side            `json:"Side"`            // 売買区分
+	CashMargin      CashMargin      `json:"CashMargin"`      // 現物信用区分
+	MarginTradeType MarginTradeType `json:"MarginTradeType"` // 信用取引区分 ※信用取引の場合必須
+	DelivType       DelivType       `json:"DelivType"`       // 受渡区分 ※株式取引の場合必須で、現物売・信用返済では「指定なし」を指定する
+	FundType        FundType        `json:"FundType"`        // 資産区分 ※株式取引の場合必須で、現物売・信用返済では「指定なし」を指定する
+	AccountType     AccountType     `json:"AccountType"`     // 口座種別
+	Qty             int             `json:"Qty"`             // 注文数量
+	ClosePositions  []ClosePosition `json:"ClosePositions"`  // 返済建玉指定
+	Price           int             `json:"Price"`           // 注文価格
+	ExpireDay       YmdNUM          `json:"ExpireDay"`       // 注文有効期限（年月日）
+	FrontOrderType  FrontOrderType  `json:"FrontOrderType"`  // 執行条件
+}
+
+// sendOrderRequestWithoutClosePositions - 返済建玉指定なしの注文発注のリクエストパラメータ
+type sendOrderRequestWithoutClosePositions struct {
+	Password           string             `json:"Password"`           // 注文パスワード
+	Symbol             string             `json:"Symbol"`             // 銘柄コード
+	Exchange           Exchange           `json:"Exchange"`           // 市場コード
+	SecurityType       SecurityType       `json:"SecurityType"`       // 商品種別
+	Side               Side               `json:"Side"`               // 売買区分
+	CashMargin         CashMargin         `json:"CashMargin"`         // 現物信用区分
+	MarginTradeType    MarginTradeType    `json:"MarginTradeType"`    // 信用取引区分 ※信用取引の場合必須
+	DelivType          DelivType          `json:"DelivType"`          // 受渡区分 ※株式取引の場合必須で、現物売・信用返済では「指定なし」を指定する
+	FundType           FundType           `json:"FundType"`           // 資産区分 ※株式取引の場合必須で、現物売・信用返済では「指定なし」を指定する
+	AccountType        AccountType        `json:"AccountType"`        // 口座種別
+	Qty                int                `json:"Qty"`                // 注文数量
+	ClosePositionOrder ClosePositionOrder `json:"ClosePositionOrder"` // 決済順序 ※信用取引の場合必須
 	Price              int                `json:"Price"`              // 注文価格
 	ExpireDay          YmdNUM             `json:"ExpireDay"`          // 注文有効期限（年月日）
 	FrontOrderType     FrontOrderType     `json:"FrontOrderType"`     // 執行条件
@@ -54,7 +138,7 @@ func (r *sendOrderRequester) Exec(request SendOrderRequest) (*SendOrderResponse,
 
 // ExecWithContext - 注文発注リクエストの実行(contextあり)
 func (r *sendOrderRequester) ExecWithContext(ctx context.Context, request SendOrderRequest) (*SendOrderResponse, error) {
-	reqBody, err := json.Marshal(request)
+	reqBody, err := request.toJSON()
 	if err != nil {
 		return nil, err
 	}
--- kabus/send_order_test.go
+++ kabus/send_order_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func Test_NewSendOrderRequester(t *testing.T) {
@@ -80,6 +81,87 @@ func Test_sendOrderRequester_Exec(t *testing.T) {
 			got1, got2 := req.Exec(SendOrderRequest{})
 			if !reflect.DeepEqual(test.want1, got1) || !reflect.DeepEqual(test.want2, got2) {
 				t.Errorf("%s error\nwant: %+v, %v\ngot: %+v, %v\n", t.Name(), test.want1, test.want2, got1, got2)
+			}
+		})
+	}
+}
+
+func Test_SendOrderRequest_toJSON(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		req  SendOrderRequest
+		want string
+	}{
+		{name: "現物ならそのまま出す",
+			req: SendOrderRequest{
+				Password:           "password",
+				Symbol:             "1320",
+				Exchange:           ExchangeToushou,
+				SecurityType:       SecurityTypeKabu,
+				Side:               SideBuy,
+				CashMargin:         CashMarginCash,
+				MarginTradeType:    MarginTradeTypeSystem,
+				DelivType:          DelivTypeUnspecified,
+				FundType:           FundTypeTransferMargin,
+				AccountType:        AccountTypeGeneral,
+				Qty:                1.0,
+				ClosePositionOrder: ClosePositionOrderUnspecified,
+				ClosePositions:     []ClosePosition{},
+				Price:              0,
+				ExpireDay:          YmdNUM{Time: time.Date(2020, 8, 24, 0, 0, 0, 0, time.Local)},
+				FrontOrderType:     FrontOrderTypeMarket},
+			want: `{"Password":"password","Symbol":"1320","Exchange":1,"SecurityType":1,"Side":"2","CashMargin":1,"MarginTradeType":1,"DelivType":2,"FundType":"AA","AccountType":2,"Qty":1,"ClosePositionOrder":-1,"ClosePositions":[],"Price":0,"ExpireDay":20200824,"FrontOrderType":10}`,
+		},
+		{name: "信用新規で返済建玉指定があれば決済順序は出さない",
+			req: SendOrderRequest{
+				Password:           "password",
+				Symbol:             "1320",
+				Exchange:           ExchangeToushou,
+				SecurityType:       SecurityTypeKabu,
+				Side:               SideBuy,
+				CashMargin:         CashMarginMarginEntry,
+				MarginTradeType:    MarginTradeTypeSystem,
+				DelivType:          DelivTypeUnspecified,
+				FundType:           FundTypeTransferMargin,
+				AccountType:        AccountTypeGeneral,
+				Qty:                1.0,
+				ClosePositionOrder: ClosePositionOrderUnspecified,
+				ClosePositions:     []ClosePosition{{HoldID: "position-id", Qty: 10}},
+				Price:              0,
+				ExpireDay:          YmdNUM{Time: time.Date(2020, 8, 24, 0, 0, 0, 0, time.Local)},
+				FrontOrderType:     FrontOrderTypeMarket},
+			want: `{"Password":"password","Symbol":"1320","Exchange":1,"SecurityType":1,"Side":"2","CashMargin":2,"MarginTradeType":1,"DelivType":2,"FundType":"AA","AccountType":2,"Qty":1,"ClosePositions":[{"HoldID":"position-id","Qty":10}],"Price":0,"ExpireDay":20200824,"FrontOrderType":10}`,
+		},
+		{name: "信用返済で返済建玉指定がなければ返済建玉指定は出さない",
+			req: SendOrderRequest{
+				Password:           "password",
+				Symbol:             "1320",
+				Exchange:           ExchangeToushou,
+				SecurityType:       SecurityTypeKabu,
+				Side:               SideBuy,
+				CashMargin:         CashMarginMarginExit,
+				MarginTradeType:    MarginTradeTypeSystem,
+				DelivType:          DelivTypeUnspecified,
+				FundType:           FundTypeTransferMargin,
+				AccountType:        AccountTypeGeneral,
+				Qty:                1.0,
+				ClosePositionOrder: ClosePositionOrderDateAscProfitDesc,
+				ClosePositions:     []ClosePosition{},
+				Price:              0,
+				ExpireDay:          YmdNUM{Time: time.Date(2020, 8, 24, 0, 0, 0, 0, time.Local)},
+				FrontOrderType:     FrontOrderTypeMarket},
+			want: `{"Password":"password","Symbol":"1320","Exchange":1,"SecurityType":1,"Side":"2","CashMargin":3,"MarginTradeType":1,"DelivType":2,"FundType":"AA","AccountType":2,"Qty":1,"ClosePositionOrder":0,"Price":0,"ExpireDay":20200824,"FrontOrderType":10}`,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := test.req.toJSON()
+			if test.want != string(got) || err != nil {
+				t.Errorf("%s error\nwant: %s\ngot: %s, %v\n", t.Name(), test.want, string(got), err)
 			}
 		})
 	}
